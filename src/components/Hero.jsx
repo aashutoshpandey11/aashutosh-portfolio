8c7ab28e0cf3d5b8bab37ee17f1f77656d9115e4
Aashutosh Pandey <aashutoshp.03@gmail.com>
2025-10-23 14:36:49 +0545

    Updated Hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,46 +6,45 @@ import profile from "../assets/Myprofile.png";
 const Hero = () => {
   return (
     <section className="hero">
+      <div className="page-container">
 
-      {/* NAVBAR */}
-      <header className="navbar">
-        <h2 className="brand">Aashutosh Pandey</h2>
+        {/* NAVBAR */}
+        <header className="navbar">
+          <h2 className="brand">A P</h2>
 
-        <ul className="nav-links">
-          <li>About</li>
-          <li>Experience</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
-      </header>
+          <ul className="nav-links">
+            <li>About</li>
+            <li>Experience</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </header>
 
-      {/* MAIN CONTENT */}
-      <div className="hero-container">
+        {/* HERO CONTENT */}
+        <div className="hero-content">
 
-        {/* IMAGE */}
-        <div className="hero-image">
-          <img src={profile} alt="Profile" />
-        </div>
-
-        {/* TEXT */}
-        <div className="hero-text">
-          <p className="intro">Hello, I'm</p>
-          <h1>Aashutosh Pandey</h1>
-          <h3>Computing Graduate | Future Cloud & AI Engineer</h3>
-
-          {/* BUTTONS */}
-          <div className="hero-buttons">
-            <button className="btn-outline">Download CV</button>
-            <button className="btn-solid">Contact Info</button>
+          {/* IMAGE */}
+          <div className="hero-image">
+            <img src={profile} alt="Aashutosh Pandey" />
           </div>
 
-          {/* SOCIAL ICONS */}
-          <div className="socials">
-            <i className="fab fa-linkedin"></i>
-            <i className="fab fa-github"></i>
-          </div>
-        </div>
+          {/* TEXT */}
+          <div className="hero-text">
+            <p className="intro">Hello, I'm</p>
 
+            <h1>
+              Aashutosh <br /> Pandey
+            </h1>
+
+            <h3>Computing Graduate | Future Cloud & AI Engineer</h3>
+
+            <div className="hero-buttons">
+              <button className="btn-outline">Download CV</button>
+              <button className="btn-solid">Contact Info</button>
+            </div>
+          </div>
+
+        </div>
       </div>
     </section>
   );
