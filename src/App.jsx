de0dfdcd20562ca5bba98c36d7d932f298a1978f
Aashutosh Pandey <aashutoshp.03@gmail.com>
2025-10-29 11:41:58 +0545

    Updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import AboutMe from './components/AboutMe';
 import Experience from './components/Experience';
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <AboutMe />
       <Experience />
       <Projects />
+      <Contact />
       {/* Add other components like Projects, Contact, etc. here */}
     </div>
   );
