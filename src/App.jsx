c87ae60f262630361cf3b646c8616f03d2cc1dd1
Aashutosh Pandey <aashutoshp.03@gmail.com>
2025-11-03 10:28:05 +0545

    Updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css'; // Your global styles
 import Hero from './components/Hero';
 import AboutMe from './components/AboutMe';
-import Experience from './components/Experience';
+import Skills from './components/Skills';
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Hero />
       <AboutMe />
-      <Experience />
+      <Skills />
       <Projects />
       <Contact />
       {/* Add other components like Projects, Contact, etc. here */}
